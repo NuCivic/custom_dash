7cc34a9c42952d92fb6e2d6f786f3a503ef35619
pj <starsinmypockets@gmail.com>
2017-05-25 14:24:55 -0400

    Initial. Update to latest custom_dash scheme.

---
+++ js/src/index.js
@@ -0,0 +1,8 @@
+/**
+ * Feel free to add more files to help you organize
+ * your code. See README.md for the Drupal.js globals
+ * that this stuff needs to be assigned to
+ **/
+import './dataHandlers.js';
+import './stateHandlers.js';
+import './devSettings.js';
